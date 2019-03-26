8c9f87504e09337e703f88c3a1e202723b5e9518
Logan Gore <gorel@purdue.edu>
2019-03-25 18:39:32 -0700

    Verification link

--- personal_site/auth/utils.py
+++ personal_site/auth/utils.py
@@ -8,8 +8,11 @@ def require_verified_email(f):
     @functools.wraps(f)
     def _wrap(*args, **kwargs):
         if not flask_login.current_user.email_verified:
+            url = flask.url_for("auth.resend_verification_email")
             flask.flash(
-                "You must verify your email before you're allowed to do that",
+                f"You must verify your email before you're allowed to do that. "
+                f"<a href='{url}'>Click here</a> to send a new verification "
+                f"link to {flask_login.current_user.email}",
                 "alert-warning",
             )
             return flask.redirect(flask.request.referrer or flask.url_for("default.home"))
